da068a2d76bb1667afeaefb6035de88b9272b976
Danilo Martins <danilo.santos.martins2018@gmail.com>
2022-09-18 14:46:29 -0300

    feat: 06 - Criando repositório de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,17 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const categoriesRepository = new CategoriesRepository();
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ name, description });
 
-  categories.push(category);
-
-  return response.status(201).json({ category });
+  return response.status(201).json();
 });
 
 export { categoriesRoutes };
