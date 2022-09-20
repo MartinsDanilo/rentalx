28eb7cc179da98de371f79b9aef93b7756d553f4
Danilo Martins <danilo.santos.martins2018@gmail.com>
2022-09-20 08:48:03 -0300

    feat: 07 - Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,21 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoriesRepository = new CategoriesRepository();
-
   categoriesRepository.create({ name, description });
 
   return response.status(201).json();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRoutes };
