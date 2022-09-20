88e9a2db226ffe2144c053c54219c35e487c4430
Danilo Martins <danilo.santos.martins2018@gmail.com>
2022-09-20 09:03:30 -0300

    feat:

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -27,6 +27,10 @@ class CategoriesRepository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
 }
 
 export { CategoriesRepository };
